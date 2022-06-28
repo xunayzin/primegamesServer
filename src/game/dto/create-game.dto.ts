6288bf115633a4001f47c17e917f3e284ba3df2a
Your Name <mxunay@gmail.com>
2022-06-27 21:42:04 -0300

    docs: implement swagger game fim da primeira semana

--- src/game/dto/create-game.dto.ts
+++ src/game/dto/create-game.dto.ts
@@ -1,7 +1,12 @@
-import { IsNumber, IsPositive } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive } from 'class-validator';
 
 export class CreateGameDto {
   @IsNumber()
   @IsPositive()
+  @ApiProperty({
+    description: 'O número do jogo',
+    example: 1,
+  })
   number: number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('PrimeGames')
+    .setDescription('Aplicação para gestão de jogos')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('game')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3333);
 }
