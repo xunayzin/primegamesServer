5b9d9e389bf98ec5c9316c22abc10cf6223e5ab9
Your Name <mxunay@gmail.com>
2022-07-08 22:43:11 -0300

    create(genre): module, service, controller, entity and dto's

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { GenreModule } from './genre/genre.module';
 
 @Module({
-  imports: [GameModule, PrismaModule],
+  imports: [GameModule, PrismaModule, GenreModule],
   controllers: [AppController],
   providers: [AppService],
 })
