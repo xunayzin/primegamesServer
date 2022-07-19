5bb5e684182e4648965fb41962514158fa9f7b3f
Your Name <mxunay@gmail.com>
2022-07-18 22:46:29 -0300

    feat(auth): implement login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
